64c6e7bfafb59b1dcaae4838d15967f607285a6c
nishi <ayutoreo0626@gmail.com>
2025-05-28 16:31:46 +0900

    5/28

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI
+from app.routers import users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS設定
+origins = ["http://localhost:3000",]
+app.add_middleware(
+    CORSMiddleware, # 第一引数でクラスを受け取って設定と共に内部的にインスタンスを生成している
+    allow_origins = origins, # 許可するオリジン
+    allow_credentials = True, # クッキーなどの情報を含めることを許可
+    allow_methods = ["*"], # 全てのHTTPメソッドを許可
+    allow_headers = ["*"], # 全てのヘッダーを許可
+)
+
+# users.routerをメインのインスタンス(app)に組み込む
+app.include_router(users.router)
--- frontend/src/compornent/HeaderWithMenu.js
+++ frontend/src/compornent/HeaderWithMenu.js
@@ -9,6 +9,27 @@ const HeaderWithMenu = () => {
     useEffect(() => {
         localStorage.setItem('activeMenuCompornent', JSON.stringify(activeMenuCompornent));
     }, [activeMenuCompornent]);
+
+    const handleLogin = async() => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/users/login', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'}, //リクエストボディがJSON形式であることを指定
+                body: JSON.stringify({/*まだ空だがここにユーザー情報など*/})
+            });
+            if(!response.ok) {
+                const errorData = await response.json();
+                //バックエンドは{"detail": "ユーザー名が..."}のように詳細なエラーメッセージをJSON形式で返してくるのでそのオブジェクトを指定している
+                throw new Error(errorData.detail || `HTTPエラー: ${response.status}`)
+            }
+            const loginData = await response.json() //.json()がPromiseを返すため非同期処理にする
+            console.log(`APIレスポンス:${loginData.message}`)
+        } catch (error) {
+            error instanceof TypeError ? 
+                alert('ネットワーク関連のエラーです') : 
+                alert(`エラー:${error.message}`)
+        }
+    }
     return (
     <>
         <div className='header-with-menu'>
@@ -18,7 +39,7 @@ const HeaderWithMenu = () => {
                 <li onClick={() => SetactiveMenuCompornent('question-to-AI')}>AIへの質問</li>
             </ul>
             <div className='user-management'>
-                <button className='btn'>ログイン</button>
+                <button className='btn' onClick={handleLogin}>ログイン</button>
                 <button className='btn'>新規登録</button>
             </div>
         </div>
