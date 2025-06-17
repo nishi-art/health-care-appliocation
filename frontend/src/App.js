00f71352c313fc8f61efa7e6a796000ffde3aeac
nishi <ayutoreo0626@gmail.com>
2025-06-17 12:16:24 +0900

    6/17

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,54 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useState } from 'react';
 import './App.css';
 import HeaderWithMenu from './compornent/HeaderWithMenu';
+import Menu from './compornent/Menu';
+import Login from './compornent/Login';
+import NewRegistration from './compornent/NewRegistration';
+import HealthCare from './compornent/HealthCare';
+import QuestionToAI from './compornent/QuestionToAI';
 
 function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
     return (
-    <>
-        <HeaderWithMenu />
-    </>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={
+                    isAuthenticated ? 
+                        <Navigate to='/menu' /> :
+                        <HeaderWithMenu />
+                }/>
+                <Route path='/login' element={
+                    isAuthenticated ? 
+                        <Navigate to='/' /> :
+                        <Login setIsAuthenticated={setIsAuthenticated} />
+                } />
+                <Route path='/register' element={
+                    isAuthenticated ? 
+                        <Navigate to='/' /> :
+                        <NewRegistration setIsAuthenticated={setIsAuthenticated} />
+                } />
+                <Route path='/menu' element={
+                    isAuthenticated ? 
+                        <>
+                            <HeaderWithMenu />
+                            <Menu />
+                        </> :
+                        <Navigate to='/' />
+                } />
+                <Route path='healthcare' element={
+                    isAuthenticated ? 
+                        <HealthCare /> :
+                        <Navigate to='/' />
+                } />
+                <Route path='/question' element={
+                    isAuthenticated ? 
+                        <QuestionToAI /> :
+                        <Navigate to='/' />
+                } />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- frontend/src/compornent/HeaderWithMenu.js
+++ frontend/src/compornent/HeaderWithMenu.js
@@ -1,52 +1,17 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import Menu from './Menu';
-import HealthCare from './HealthCare';
-import QuestionToAI from './QuestionToAI';
-import Login from './Login';
-import NewRegistration from './NewRegistration';
+import { useNavigate } from 'react-router-dom';
 
 const HeaderWithMenu = () => {
-    const [activeMenuCompornent, SetactiveMenuCompornent] = useState(() => 
-        JSON.parse(localStorage.getItem('activeMenuCompornent')) || '' );
-    const [visibleLogin, setVisibleLogin] = useState(() => 
-        JSON.parse(localStorage.getItem('visibleLogin')) ?? false);
-    const [visibleNewRegistration, setVisibleNewRegistration] = useState(() => 
-        JSON.parse(localStorage.getItem('visibleNewRegistration')) ?? false);
-    const [isRegistered, setIsRegistered] = useState(/*
-        JSON.stringify(localStorage.getItem('isRegistered')) ?? */false);
-
-    useEffect(() => {
-        localStorage.setItem('activeMenuCompornent', JSON.stringify(activeMenuCompornent));
-        localStorage.setItem('visibleLogin', JSON.stringify(visibleLogin));
-        localStorage.setItem('visibleNewRegistration', JSON.stringify(visibleNewRegistration));
-    }, [activeMenuCompornent, visibleLogin, visibleNewRegistration]);
-
+    const navigate = useNavigate();
     return (
     <>
         <div className='header-with-menu'>
             <h1 className='header'>Pet Health Care</h1>
-            {!isRegistered && 
-                <div className='user-management'>
-                    <button className='btn' onClick={() => {setVisibleLogin(true);setVisibleNewRegistration(false);}}>ログイン</button>
-                    <button className='btn' onClick={() => {setVisibleNewRegistration(true);setVisibleLogin(false);}}>新規登録</button>
-                </div>
-            }
-            {isRegistered &&
-                <Menu 
-                activeMenuCompornent={activeMenuCompornent} 
-                SetactiveMenuCompornent={SetactiveMenuCompornent}
-                />
-            }
-            {!isRegistered && visibleLogin && 
-                <Login setIsRegistered={setIsRegistered} />
-            }
-            {!isRegistered && visibleNewRegistration && 
-                <NewRegistration setIsRegistered={setIsRegistered} />
-            }
+            <div className='user-management'>
+                <button className='btn' onClick={() => navigate('/login')}>ログイン</button>
+                <button className='btn' onClick={() => navigate('/register')}>新規登録</button>
+            </div>
         </div>
-        {activeMenuCompornent === 'health-care' ?? <HealthCare />}
-        {activeMenuCompornent === 'question-to-AI' ?? <QuestionToAI />}
     </>
     )
 }
