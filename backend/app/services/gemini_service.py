94697b8462120eec67bf498f840c565b47a73eb0
nishi <ayutoreo0626@gmail.com>
2025-08-05 11:16:53 +0900

    8/5

--- backend/app/services/gemini_service.py
+++ backend/app/services/gemini_service.py
@@ -2,6 +2,8 @@ import textwrap
 import google.generativeai as genai
 import os
 
+api_key = os.getenv("GOOGLE_API_KEY")
+
 def print_text(text):
     text = text.replace('・', ' *')
     indented_text = ""
@@ -9,7 +11,7 @@ def print_text(text):
         indented_text += f"> {line}\n"
     print(indented_text)
 
-genai.configure(api_key="***REMOVED***")
+genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-2.5-pro')
 response = model.generate_content("買っているトイプードルの体重が1か月で5kg増えたのですが食事の量を減らした方が良いですか？")
 print_text(response.text)
