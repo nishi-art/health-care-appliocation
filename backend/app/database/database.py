5add4f63da88231ab64025f05fee7d71d97335cf
nishi <ayutoreo0626@gmail.com>
2025-11-09 11:32:11 +0900

    11/9

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,12 +1,19 @@
 from sqlalchemy import create_engine   # SQLAlchemyでデータベース接続を確立するための関数create_engineを使うため
 from sqlalchemy.orm import DeclarativeBase   # SQLAlchemyのORMモデルを作成するための基底クラスを生成する関数を使うため
 from sqlalchemy.orm import sessionmaker   # データベースセッションを作成するためのファクトリクラス
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+load_dotenv
+
+SQLALCHEMY_DATABASE_URL = os.getenv("AWS_RDS_ENDPOINT")
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("環境変数 AWS_RDS_ENDPOINT が設定されていません")
 
 # SQLiteとPythonプログラムを接続し管理するためのデータベースエンジンをエンジンオブジェクトとして作成
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 # データベースセッション（インスタンス）を作成するためのファクトリを定義
 # 左から　トランザクションを手動で制御するため, セッションの変更を手動で制御するため, 作成したエンジンとセッションを関連付け（どのデータベースに接続するかを指定）
@@ -16,6 +23,7 @@ class Base(DeclarativeBase):
     pass
 
 def init_db():
+    from . import models
     Base.metadata.create_all(bind=engine)
 
 def get_db():
--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, DateTime
 from datetime import datetime
 from .database import Base
 
@@ -11,12 +11,14 @@ class User(Base):
     user_id: Mapped[str] = mapped_column(unique=True, index=True)
     hashed_password: Mapped[str]
     created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True)
         server_default=func.now()
-    )  # SQLiteを使っているのでtimezone=Trueは削除
+    )
     updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True)
         server_default=func.now(),
         onupdate=func.now()
-    ) # 同じ理由でtimezone=Trueは削除
+    )
     memos: Mapped["Memo"] = relationship("Memo", back_populates="user")
 
 class Memo(Base):
